dca2e4617d6c152b81b6ac1fd969b935c5a5df65
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-10 11:49:25 -0500

    set initialFormState

--- src/RSVPForm.js
+++ src/RSVPForm.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
 
-function RSVPForm() {}
+function RSVPForm() {
+  const initialFormState = {};
+  const [formData, setFormData] = useState({ ...initialFormState });
+  const handleChange = ({ target }) => {
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    setFormData({
+      ...formData,
+      [target.name]: value,
+    });
+  };
+}
 
 export default RSVPForm;
